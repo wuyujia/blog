5b5e75c5ef454b78f9964e9cc0686c447b88eeb6
WuYujia <365990665@qq.com>
2017-01-07 00:52:27 +0800

    commmit

---
+++ js/header.js
@@ -0,0 +1,9 @@
+$("head").html("<meta http-equiv='Expires' content='0'>" +
+"<meta http-equiv='Pragma' content='no-cache'>" +
+"<meta http-equiv='Cache-control' content='no-cache'>" +
+"<meta http-equiv='Cache' content='no-cache'>" +
+"<title>Mr.Black's Blog</title>" +
+"<link rel='stylesheet' href='https://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css' />" +
+"<link rel='stylesheet' href='css/header.css' />" +
+// "<script src='https://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js'></script>" +
+"<script src='https://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js'></script>")
