7889807f8742b165e76b4b65126166b5a204d0e7
wuyujia <wuyujia@beiquan.org>
2017-01-07 08:45:01 +0800

    commit

--- js/header.js
+++ js/header.js
@@ -4,6 +4,6 @@ $("head").html("<meta http-equiv='Expires' content='0'>" +
 "<meta http-equiv='Cache' content='no-cache'>" +
 "<title>Mr.Black's Blog</title>" +
 "<link rel='stylesheet' href='https://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css' />" +
-"<link rel='stylesheet' href='css/header.css' />" +
+"<link rel='stylesheet' href='https://wuyujia.github.io/blog/css/header.css' />" +
 // "<script src='https://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js'></script>" +
 "<script src='https://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js'></script>")
